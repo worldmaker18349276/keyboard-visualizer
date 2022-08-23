05300191565b244bb6ae5bee9ce0248c2265c2cb
worldmaker <worldmaker18349276@gmail.com>
2022-08-23 16:14:34 +0800

    fix typo, bash script

--- keyboard-visualizer.sh
+++ keyboard-visualizer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PYTHON_PATH=$(which python3)
-COLS=$(tput cols)
-LINES=$(tput lines)
+PREV_COLS=$(tput cols)
+PREV_LINES=$(tput lines)
 resize -s 8 56 &>/dev/null
 sudo $PYTHON_PATH keyboard_visualizer.py
-resize -s $LINES $COLS &>/dev/null
+resize -s $PREV_LINES $PREV_COLS &>/dev/null
